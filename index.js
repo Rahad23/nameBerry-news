3ead671bf0166798d11be49acbec68a862b219eb
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2022-09-03 02:10:37 +0600

    add reloder

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ firstApi();
 // get second navbar div id
 
 const UlsecondNave = document.getElementById('ulSecondNav');
-
+const spenars = document.getElementById('reloder');
 const reciveData = data =>{
     const getApiArray = data?.data.news_category;
     getApiArray?.forEach(element => {
@@ -23,8 +23,10 @@ const reciveData = data =>{
         UlsecondNave.appendChild(createLi);
 
         document.getElementById(category_id).addEventListener('click', function(){
+            // Add spenars
+            spenars.classList.remove('d-none');
         const categoriSearch = () =>{
-            fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+             fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
             .then(req => req.json(req))
             .then(data => search(data))
            };
@@ -36,15 +38,20 @@ const reciveData = data =>{
             if(data?.status === false){
                 parentDiv.innerHTML = ``;
                 message.classList.remove('d-none');
+                // spenar reloader
+                spenars.classList.add('d-none');
             }else{
                 parentDiv.innerHTML = ``;
                 message.classList.add('d-none');
+                
             }
 
             const newsArry = data?.data;
+            // newsArry.sort()
             newsArry?.forEach(element =>{
+                // console.log(element);
             const {author, details, image_url, others_info, rating, thumbnail_url, title, total_view, _id} = element;
-            console.log(rating);
+            // console.log(_id);
 
             if(details.length >250){
 
@@ -74,7 +81,7 @@ const reciveData = data =>{
                     </div>
                     <div class="col-3 d-flex justify-content-center align-items-center">
                         <i class="fa-solid fa-eye mx-2" style="font-size: 18px;"></i>
-                        <span style="font-weight: 700; color: #2b1e1e;">${total_view}</span>
+                        <span style="font-weight: 700; color: #2b1e1e;">${total_view===null  ? "No view": total_view}</span>
                     </div>
                     <div class="col-3 d-flex justify-content-center align-items-center">
                         <i class="fa-regular fa-star-half-stroke" style="margin-left: 5px; font-size: 20px; color: #515151;"></i>
@@ -84,7 +91,7 @@ const reciveData = data =>{
                         <i class="fa-regular fa-star" style="margin-left: 5px; font-size: 20px; color: #515151;"></i>
                     </div>
                     <div class="col-3 d-flex justify-content-end align-items-center p-5">
-                        <i class="fa-solid fa-right-long" style="font-size: 18px; color: #5D5FEF;"></i>
+                     <i  onclick="modal('${_id}')" class="fa-solid fa-right-long" style="font-size: 18px; color: #5D5FEF; cursor: pointer;" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
                     </div>
                   </div>
                 </div>
@@ -94,10 +101,10 @@ const reciveData = data =>{
             `;
                 createDiv.innerHTML = bootsCard;
                 parentDiv.appendChild(createDiv);
-
-                console.log(element);
+                spenars.classList.add('d-none');
             }
             });
+            // console.log(element);
            }
            search();
         });
@@ -105,6 +112,16 @@ const reciveData = data =>{
     });
 
 }
+
+const modal = id =>{
+    fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+    .then(req => req.json())
+    .then(data => console.log(data))
+    // console.log(id);
+}
+
+
+modal( );
 reciveData();
 
 
